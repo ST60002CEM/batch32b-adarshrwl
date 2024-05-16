d04ac172b7b00b6845213352a7bf7ffdcee27fd5
Adarsh Rawal <adarshrawal321@gmail.com>
2024-05-16 13:02:33 +0545

    completed basic layout of login

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,50 +1,99 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   static const mybox = SizedBox(
-    height: 10,
+    height: 25,
   );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-        backgroundColor: Colors.amber[100],
-        elevation: 0,
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          mybox,
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: "Username/Email",
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red))),
-          ),
-          mybox,
-          TextFormField(
-            decoration: const InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red))),
-          ),
-          mybox,
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 101, 249, 106),
+        appBar: AppBar(
+          title: const Text('Login'),
+          backgroundColor: Colors.amber[100],
+          elevation: 0,
+          centerTitle: true,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            mybox,
+            TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "Username/Email",
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red))),
+            ),
+            mybox,
+            TextFormField(
+              decoration: const InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.red))),
+            ),
+            mybox,
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 101, 249, 106),
+                  ),
+                  child: const Text("Login")),
+            ),
+            mybox,
+            RichText(
+                text: TextSpan(
+              children: [
+                const TextSpan(
+                  text: 'Don\'t have account? ',
+                  style: TextStyle(color: Colors.black),
                 ),
-                child: const Text("Login")),
-          )
-        ],
-      ),
-    );
+                TextSpan(
+                  text: 'Sign up',
+                  style: const TextStyle(
+                    color: Colors.blue, // You can customize the color
+                    fontWeight: FontWeight.bold,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      print('Sign up button tapped');
+                    },
+                ),
+              ],
+            )),
+            mybox,
+            const Text("Forget Password?"),
+            mybox,
+            Center(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(CircleBorder())),
+                      child: CircleAvatar(
+                          child: Image.asset('assets/icons/google.png'))),
+                  const SizedBox(
+                    width: 30,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(CircleBorder())),
+                      child: CircleAvatar(
+                          child: Image.asset('assets/icons/facebook.png')))
+                ],
+              ),
+            )
+          ]),
+        ));
   }
 }
