36b5c151db45072870b4bee519448d51f9228875
Adarsh Rawal <adarshrawal321@gmail.com>
2024-06-21 10:47:44 +0545

    added auth remote data source

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -9,5 +9,6 @@ class ApiEndpoints {
 
   // ====================== Auth Routes ======================
   static const String login = "user/login";
-  static const String register = "user/create";
+  static const String register = "user/register";
+  static const String getCurrentUser = "user/me";
 }
---
+++ lib/features/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -0,0 +1,131 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:snapdwell/app/constants/api_endpoints.dart';
+import 'package:snapdwell/core/failure/failure.dart';
+import 'package:snapdwell/core/networking/remote/http_service.dart';
+import 'package:snapdwell/core/shared_prefs/user_shared_prefs.dart';
+import 'package:snapdwell/features/auth/data/dto/get_current_user_dto.dart';
+import 'package:snapdwell/features/auth/domain/entity/auth_entity.dart';
+
+final authRemoteDataSourceProvider = Provider(
+  (ref) => AuthRemoteDataSource(
+    dio: ref.read(httpServiceProvider),
+    userSharedPrefs: ref.read(userSharedPrefsProvider),
+  ),
+);
+
+class AuthRemoteDataSource {
+  final Dio dio;
+  final UserSharedPrefs userSharedPrefs;
+
+  AuthRemoteDataSource({required this.dio, required this.userSharedPrefs});
+
+  Future<Either<Failure, bool>> registerStudent(AuthEntity user) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.register,
+        data: {
+          "id":user.id,
+          "email": user.email,
+          "username": user.username,
+          "phone": user.phone,
+          "password": user.password,
+          "accountType": user.accountType,
+        },
+      );
+      if (response.statusCode == 200) {
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+
+
+  Future<Either<Failure, bool>> loginStudent(
+    String username,
+    String password,
+  ) async {
+    try {
+      Response response = await dio.post(
+        ApiEndpoints.login,
+        data: {
+          "username": username,
+          "password": password,
+        },
+      );
+      if (response.statusCode == 200) {
+        String token = response.data["token"];
+        // Save token to shared prefs
+        await userSharedPrefs.setUserToken(token);
+        return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+
+  Future<Either<Failure, AuthEntity>> getCurrentUser() async {
+    try {
+      // Get the token from shared prefs
+      String? token;
+      var data = await userSharedPrefs.getUserToken();
+      data.fold(
+            (l) => token = null,
+            (r) => token = r!,
+      );
+
+      var response = await dio.get(
+        ApiEndpoints.getCurrentUser,
+        options: Options(headers: {
+          'Authorization': 'Bearer $token',
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        GetCurrentUserDto getCurrentUserDto = GetCurrentUserDto.fromJson(response.data);
+
+        return Right(getCurrentUserDto.toEntity());
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+}
